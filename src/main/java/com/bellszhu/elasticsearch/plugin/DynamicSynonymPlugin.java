7f5e8cbb0c78e11b39c7489b8f56d0465d6a13b1
lxc-123 <71161685+lxc-123@users.noreply.github.com>
2021-04-01 15:29:23 +0800

    Update DynamicSynonymPlugin.java

--- src/main/java/com/bellszhu/elasticsearch/plugin/DynamicSynonymPlugin.java
+++ src/main/java/com/bellszhu/elasticsearch/plugin/DynamicSynonymPlugin.java
@@ -13,6 +13,7 @@ import org.elasticsearch.plugins.Plugin;
 
 import java.util.HashMap;
 import java.util.Map;
+import static org.elasticsearch.plugins.AnalysisPlugin.requiresAnalysisSettings;
 
 
 /**
